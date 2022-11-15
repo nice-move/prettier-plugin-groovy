6eca71d9fcbe348b3b235ad64c96eab07e784324
Eric <airkro@qq.com>
2022-11-15 16:06:42 +0800

    feat: init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# prettier-plugin-groovy
+
+A [Prettier] plugin for Groovy files.
+
+[prettier]: https://prettier.io/
+
+[![npm][npm-badge]][npm-url]
+[![github][github-badge]][github-url]
+![node][node-badge]
+
+[npm-url]: https://www.npmjs.com/package/prettier-plugin-groovy
+[npm-badge]: https://img.shields.io/npm/v/prettier-plugin-groovy.svg?style=flat-square&logo=npm
+[github-url]: https://github.com/nice-move/prettier-plugin-groovy
+[github-badge]: https://img.shields.io/npm/l/prettier-plugin-groovy.svg?style=flat-square&colorB=blue&logo=github
+[node-badge]: https://img.shields.io/node/v/prettier-plugin-groovy.svg?style=flat-square&colorB=green&logo=node.js
+
+## Installation
+
+```bash
+npm install prettier-plugin-groovy --save-dev
+```
+
+## Usage
+
+```sh
+prettier --write jenkinsfile
+```
+
+## Related
+
+- [@nice-move/prettier-config](https://github.com/nice-move/nice-move/tree/master/packages/prettier-config)
+- [@nice-move/prettier-plugin-package-json](https://github.com/nice-move/prettier-plugin-package-json)
+- [@nice-move/prettier-plugin-ssh-config](https://github.com/nice-move/prettier-plugin-ssh-config)
---
+++ test/test.mjs
@@ -0,0 +1,55 @@
+import test from 'ava';
+import prettier from 'prettier';
+
+function pretty(string, options) {
+  return prettier.format(string, {
+    plugins: ['.'],
+    ...options,
+  });
+}
+
+const source = `
+def bumpVersion(String target,    String version_type, Boolean reset =   false) {    def
+  versionMap =
+  ['major':0, 'minor' : 1, 'patch':   2]
+              def versionArray = target.findAll(/\\d+\\.\\d+\\.\\d+/)[0].tokenize('.')
+          try
+  {        def   index =     versionMap.get(version_type);
+  versionArray[index] =versionArray[index].toInteger() + 1
+  if(   reset )
+  {
+      for(int i=2;i>index;     i--) {
+          versionArray[i]    =    0            }        }
+  } catch(       Exception e) {        println("Unrecognized version type \\"version_type\\" (should be major, minor or patch)")    }
+  return             versionArray.join(                   '.'                       )
+  }
+  println(bumpVersion('1.2.3', 'minor', true))
+`;
+
+test('format by filename', (t) => {
+  const result = pretty(source, { filepath: 'jenkinsfile' });
+
+  t.snapshot(result);
+
+  t.is(result, pretty(source, { filepath: 'Jenkinsfile' }));
+  t.is(result, pretty(source, { filepath: 'a.jenkinsfile' }));
+  t.is(result, pretty(source, { filepath: 'a.Jenkinsfile' }));
+});
+
+test('format by parser', (t) => {
+  const result = pretty(source, { parser: 'groovy' });
+
+  t.snapshot(result);
+});
+
+test('format in markdown', (t) => {
+  const text = `
+\`\`\`groovy
+${source}
+\`\`\`
+`;
+
+  const result = pretty(text, { filepath: 'fake.md' });
+
+  t.snapshot(result);
+});
